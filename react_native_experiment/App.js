70a5d5c6cfa1cbd3351c1d82d32270aef87296f6
Steven van Stiphout <steven.vanstiphout@deltadna.com>
2018-07-02 12:35:59 +0100

    added some buttons and elements into the app

--- react_native_experiment/App.js
+++ react_native_experiment/App.js
@@ -1,58 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, {Component} from 'react';
+import {AppRegistry, Button, Text, TextInput, View, Alert} from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class StevensAppComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+    _benassiButton() {
+        Alert.alert('And then just touch me!');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    }
+
+    render() {
+        return (
+            // Try setting `alignItems` to 'flex-start'
+            // Try setting `justifyContent` to `flex-end`.
+            // Try setting `flexDirection` to `row`.
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                //justifyContent: 'flex-end',
+                alignItems: 'flex-start',
+            }}>
+                <View>
+                    <Text style={{padding: 10, fontSize: 24}}>
+                        This is a React Native test app
+                    </Text>
+                </View>
+                <Button
+                    onPress={this._benassiButton()}
+                    title="Push me!"
+                />
+
+                <TextInput
+                    style={{height: 40, width: 250}}
+                    placeholder="Type here to translate!"
+                    onChangeText={(text) => this.setState({text})}
+                />
+                <Text style={{padding: 10, fontSize: 24}}>
+                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+                </Text>
+
+                <View style={{width: 50, height: 50, padding: 10, backgroundColor: 'powderblue'}}>
+                    <Text>hi 1</Text>
+                </View>
+                <View style={{width: 50, height: 50, padding: 10, backgroundColor: 'skyblue'}}>
+                    <Text>hi 2</Text>
+                </View>
+                <View style={{width: 50, height: 50, padding: 10, backgroundColor: 'steelblue'}}>
+                    <Text>hi 3</Text>
+                </View>
+            </View>
+        );
+    }
+};
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => StevensAppComponent);
